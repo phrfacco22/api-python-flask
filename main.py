ae1608600d117c8120c6d3376ec057b992431235
pfacco <pfacco@redegazeta.com.br>
2024-07-03 18:20:31 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,85 @@
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# with open('tasks.json', 'r', encoding='utf8') as file:
+#     data = file.read()
+
+# obj = json.loads(data)
+
+# with open('employees.json', 'r', encoding='utf8') as file:
+#     data_employees = file.read()
+    
+
+employees = [
+        { "id": 1, "name": "Ashley" }, 
+        { "id": 2, "name": "Kate" }, 
+        { "id": 3, "name": "Joe" }
+    ]
+
+nextEmployeeId = 4
+3
+
+
+@app.route('/employees', methods=['GET'])
+def get_employees():
+    return jsonify(employees)
+
+@app.route('/employees/<int:id>', methods=['GET'])
+def get_employee_by_id(id: int):
+    employee = get_employee(id)
+    if employee is None:
+        return jsonify({ 'error': 'Funcionário não encontrado'}), 404
+    return jsonify(employee)
+
+def get_employee(id):
+    return next((e for e in employees if e['id'] == id), None)
+
+def employee_is_valid(employee):
+    for key in employee.keys():
+        if key != 'name':
+            return False
+    return True
+
+@app.route('/employees', methods=['POST'])
+def insert_employee():
+    global nextEmployeeId
+    employee = json.loads(request.data)
+    if not employee_is_valid(employee):
+        return jsonify({ 'error': 'Propriedade(s) do funcionário inválida.'}), 400    
+    
+    employee['id'] = nextEmployeeId
+    nextEmployeeId += 1
+    employees.append(employee)
+    
+    return '', 201, {'location': f'/employees/{employee["id"]}'}
+
+
+@app.route('/employees/<int:id>', methods=['PUT'])
+def update_employee(id: int):
+    employee = get_employee(id)
+    if employee is None:
+        return jsonify({'error': 'Funcionário não encontrado'}), 404
+    
+    updated_employee = (request.data)
+    if not employee_is_valid(updated_employee):
+        return jsonify({ 'error': 'Propriedade(s) do funcionário inválida.' }), 400
+
+    employee.update(updated_employee)
+    
+    return jsonify(employee)
+    
+@app.route('/employees/<int:id>', methods=['DELETE'])
+def delete_employee(id: int):
+    global employees
+    employee = get_employee(id)
+    if employee is None:
+        return jsonify({ 'error': 'Funcionário não encontrado' }), 404
+    
+    employees = [e for e in employees if e['id'] != id]    
+    return jsonify(employee), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
